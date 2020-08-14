d77d9d23e4d20d8ca4567ead69f6fc16c5608cf2
Milan Nakic <milan.nakic@gmail.com>
2020-08-15 01:24:26 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from "react";
 import "./App.css";
+import {
+  Layout,
+  Header,
+  Textfield,
+  Drawer,
+  Navigation,
+  Content,
+} from "react-mdl";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div style={{ height: "300px", position: "relative" }}>
+      <Layout fixedHeader>
+        <Header
+          title={
+            <span>
+              <span style={{ color: "#ddd" }}>Area / </span>
+              <strong>The Title</strong>
+            </span>
+          }
+        >
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content />
+      </Layout>
     </div>
   );
 }
